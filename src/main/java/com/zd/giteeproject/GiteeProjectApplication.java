508dfe1d2748ca134c422d67249abdfa6f4b033a
luxinlong <xinlong.lu@zd-automotive.cn>
2021-09-13 10:49:31 +0800

    代码上传

---
+++ src/main/java/com/zd/giteeproject/GiteeProjectApplication.java
@@ -0,0 +1,13 @@
+package com.zd.giteeproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiteeProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GiteeProjectApplication.class, args);
+    }
+
+}
